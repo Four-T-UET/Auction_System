49642c0dd90e1331c4b3fd69618add8d43822e87
BuiDuyThang <25022017@vnu.edu.vn>
2026-05-30 23:53:58 +0700

    CI

--- sever/src/main/java/sever/handler/AuthHandler.java
+++
@@ -1,8 +0,0 @@
-package sever.handler;
-
-import java.io.ObjectOutputStream;
-
-public interface AuthHandler {
-  // Nhận mảng dữ liệu và stream để phản hồi object
-  void handle(String[] parts, ObjectOutputStream out);
-}
